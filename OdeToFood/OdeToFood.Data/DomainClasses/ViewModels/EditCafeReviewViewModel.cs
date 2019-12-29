7a56534e6fb1886e359d59413fce85db97d63223
gijsclaes <11500418@student.pxl.be>
2019-12-29 14:17:51 +0100

    tests run

--- OdeToFood/OdeToFood.Data/DomainClasses/ViewModels/EditCafeReviewViewModel.cs
+++ OdeToFood/OdeToFood.Data/DomainClasses/ViewModels/EditCafeReviewViewModel.cs
@@ -6,6 +6,9 @@ namespace OdeToFood.Data.DomainClasses.ViewModels
 {
     public class EditCafeReviewViewModel : EditReviewViewModel
     {
+<<<<<<< HEAD
+=======
         
+>>>>>>> 29f786c762c6c3942826b06e9227fea83a2c2ff4
     }
 }
